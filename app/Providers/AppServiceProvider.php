20ebcb287bc60ae4c923334cfccf1e508f82d491
Deyand03 <defryyandy7@gmail.com>
2025-11-25 23:07:30 +0700

    asomaso

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // LOGIC PAKSA HTTPS DI VERCEL
+        // Kalau environment-nya bukan 'local', paksa semua link jadi HTTPS
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
